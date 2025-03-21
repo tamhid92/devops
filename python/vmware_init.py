8ec54baa4d883787ad62a362f8ce6388665ac6a2
Tamhid Chowdhury <tc5g@mtmail.mtsu.edu>
2025-03-21 13:24:11 -0500

    vmware refactor

--- python/vmware_init.py
+++ python/vmware_init.py
@@ -1,7 +1,9 @@
-from hvac_lib import HVACClient
-import vm_rest_lib
+from jinja2 import Template
 import argparse
-import os
+import os, sys
+sys.path.append("../lib")
+from hvac_lib import HVACClient
+import vm_lib as vm_lib
 
 #Pass node name as an arg
 parser = argparse.ArgumentParser(description ='VMWare VM Name')
@@ -16,7 +18,35 @@ def get_password(account_name):
         pwd = value
     return [username, pwd]
 
+def get_vm_info():
+    vm_info = {}
+    vm_acc = get_password('vmware_rest')
+    vm_lib.authenticate(vm_acc[0],vm_acc[1])
+    vms = vm_lib.get_vms()
+    for vm in vms:
+        if vm['path'].split("\\")[-1].split(".")[0] == args.node_name:
+            vm_ip = vm_lib.get_ip(vm['id'])
+            vm_info = {
+                "id"    : vm['id'],
+                "name"  : vm['path'].split("\\")[-1].split(".")[0],
+                "ip"    : vm_ip['ip']
+            }
+    return vm_info
+
+def crete_from_template(ip, sudo_pwd):
+    with open("../templates/hosts.tmp",'r') as file:
+        content = file.read()
+    template = Template(content) 
+    rendered_form = template.render(
+        ip_addr=ip,
+        sudo_pass=sudo_pwd
+    )
+    with open("hosts.ini",'w') as file:
+        file.write(rendered_form)
+
 def main():
-    pwd = get_password('postgres')
+    vm_info = get_vm_info()
+    state = vm_lib.get_power(vm_info["id"])
+    print(state)
 
 main()
